8794c72af13b662813503449e1ad4ac536f2059d
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-09-13 23:46:41 -0300

    Refactor cart test to validate product quantity increase and improve setup

--- tests/cart/business-rules.spec.ts
+++ tests/cart/business-rules.spec.ts
@@ -2,29 +2,29 @@ import { test, expect } from "@playwright/test";
 
 import { login } from "../helper/auth/login";
 import user from "../fixtures/users.json" with { type: "json" };
+import { checkProductCart } from "../helper/cart/cart";
 
 test.describe("Business rules", () => {
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page, context }) => {
+    context.clearCookies();
     await page.goto("/login");
-
     await login(page, user);
   });
 
   test.describe("Positive scenarios", () => {
-    test("BR-001 - Add a product to the cart successfully", async ({
+    test("BR-002 - Increase the quantity of a product in the cart", async ({
       page,
     }) => {
-      const cartMenu = ["Item", "Description", "Price", "Quantity", "Total"];
+      const initialQuantity = Number(page.locator(".disabled").textContent());
+      const curretQuantity = Number(initialQuantity + 1);
+
       await page.locator("a.add-to-cart").first().click();
 
       await expect(page.getByText(/Product.*added/i)).toBeVisible();
       await page.getByRole("link", { name: "View Cart" }).click();
+      checkProductCart(page);
 
-      for (let menu of cartMenu) {
-        await expect(
-          page.locator(".table.table-condensed").locator(".cart_menu"),
-        ).toContainText(menu);
-      }
+      expect(Number(initialQuantity)).toBe(curretQuantity);
     });
   });
 });
