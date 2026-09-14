93ad60bc9360b2d40726bdcf4f37f68ff0b340dc
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-09-13 23:49:18 -0300

    Add initial cart test to validate product addition and cart visibility

---
+++ tests/cart/happy-path.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+import { login } from "../helper/auth/login";
+import user from "../fixtures/users.json" with { type: "json" };
+import { checkProductCart } from "../helper/cart/cart";
+
+test.describe("Business rules", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/login");
+
+    await login(page, user);
+  });
+
+  test.describe("Positive scenarios", () => {
+    test("BR-001 - Add a product to the cart successfully", async ({
+      page,
+    }) => {
+      await page.locator("a.add-to-cart").first().click();
+
+      await expect(page.getByText(/Product.*added/i)).toBeVisible();
+      await page.getByRole("link", { name: "View Cart" }).click();
+      checkProductCart(page);
+    });
+  });
+});
